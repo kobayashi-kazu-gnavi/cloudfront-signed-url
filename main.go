60040d7650f773098a7abf4363b37a19463e12af
kobayashi-kazu <kobayashi-kazu@gnavi.co.jp>
2023-09-12 15:20:30 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/service/cloudfront/sign"
+	"time"
+)
+
+const (
+	privateKeyString = `-----BEGIN RSA PRIVATE KEY-----
+-----END RSA PRIVATE KEY-----
+`
+	keyPairID = "keyPairID"
+	region    = "region"
+	url       = "url"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+// handler lambda handler.
+func handler(ctx context.Context) {
+	// convert to rsa.PrivateKey
+	block, _ := pem.Decode([]byte(privateKeyString))
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		fmt.Printf("failed to parse private key: %v\n", err)
+		return
+	}
+
+	// create signer
+	signer := sign.NewURLSigner(keyPairID, privateKey)
+
+	// create signed url
+	signedURL, err := signer.Sign(url, time.Now().Add(24*time.Hour))
+
+	fmt.Printf("signed url: %s\n", signedURL)
+}
